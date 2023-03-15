e1bfeef1f287203c0b5a944f7abc9334fde52224
Ronald de Rooij <ronaldderooij17 [at] gmail.com>
2023-03-15 18:06:47 +0100

    Changes before clearing laptop

--- components/RdrButton.tsx
+++ components/RdrButton.tsx
@@ -1,9 +1,9 @@
-import Link from "next/link"
+import Link from 'next/link'
 
 interface Props {
-  classNames?: string,
-  url: string,
-  link: string,
+  classNames?: string
+  url: string
+  link: string
   onClick?: () => void
 }
 
@@ -12,7 +12,8 @@ export default function RdrButton({ classNames, url, link, onClick }: Props) {
     <Link href={url} legacyBehavior>
       <a
         onClick={onClick}
-        className={`px-4 py-2 border border-transparent font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ${classNames}`}>
+        className={`px-4 py-2 border border-transparent font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ${classNames}`}
+      >
         {link}
       </a>
     </Link>
